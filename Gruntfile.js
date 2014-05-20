1bc392f9c20b3193de376cbcb414a073efa7c431
Fabrício Silva <fabricio@sgec.com.br>
2014-05-20 09:43:55 -0300

    configure Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,50 @@
 "use strict";
+
 module.exports = function( grunt ) {
+
+  // matchdep -> Remove a necessidade de usar o loadNpmTasks
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
     // configuracoes das tarefas
-  });
 
-  // carregando plugins
-  grunt.loadNpmTasks( 'plugin-name' );
+    // Watch
+    watch: {
+      css: {
+        files: [ '../assets/scss/**/*' ],
+        tasks: [ 'compass' ]
+      },
+      js: {
+        files: '../assets/js/**/*',
+        tasks: [ 'uglify' ]
+      }
+    },
+
+    // Compile scss
+    compass: {
+      dist: {
+        options: {
+          force: true,
+          config: 'config.rb',
+          outputStyle: 'compressed'
+        }
+      }
+    },
+
+    // Concat and minify javascripts
+    uglify: {
+      options: {
+        mangle: false
+      },
+      dist: {
+        files: {
+          '../build/js/app.min.js': [
+            '../assets/js/app.js'
+          ]
+        }
+      }
+    },
+  });
 
   // registrando tarefas
   grunt.registerTask( 'default', [ 'watch' ] );
